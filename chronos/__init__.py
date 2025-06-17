e907bdfbf1a30085d022249ff7e2b7e4208f27ce
Leo Guinan <leo.guinan@gmail.com>
2025-06-17 14:46:23 -0400

    adding personality

--- chronos/__init__.py
+++ chronos/__init__.py
@@ -1,16 +1,20 @@
 """chronos SDK public API"""
-from .change_algebra import ChangeEvent, ChangeSet, change_set
+from .change_algebra import ChangeEvent, ChangeSet
 from .trust import TrustGraph
-from .manifold import Metric, geodesic, curvature
+from .manifold import InnovationMetric
+from .future import FutureChangeSet
 from .navigator import Navigator
 
 __all__ = [
+    "change_algebra",
+    "trust",
+    "manifold",
+    "future",
+    "navigator",
     "ChangeEvent",
     "ChangeSet",
-    "change_set",
+    "FutureChangeSet",
     "TrustGraph",
-    "Metric",
-    "geodesic",
-    "curvature",
+    "InnovationMetric",
     "Navigator",
 ] 
